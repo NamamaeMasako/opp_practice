c58f42344043e57e66567ded244a1293abc4f651
NamamaeMasako <c23026730@gmail.com>
2022-03-22 16:34:28 +0800

    增加註解

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import Circle from './circle';
 import Square from './square';
 import Triangle from './triangle';
+//圓形
 let C = new Circle(5);
 C.getArea();
+//正方形
 let S = new Square(5);
 S.getArea();
+//正三角形
 let T = new Triangle(5);
 T.getArea();
